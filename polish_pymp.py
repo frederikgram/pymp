42349be9f1d032bd5eeb1dc1a4337958968db6a7
frederikgram <frederikxyz@hotmail.com>
2018-11-12 09:40:09 +0100

    Refactor

--- polish_pymp.py
+++ polish_pymp.py
@@ -1,22 +1,13 @@
-func_map = {"+": lambda a, b: a + b,
-            "*": lambda a, b: a * b}
-func_keys = list(func_map.keys())
-
-
 def parse(tokens):
     """ Recursively parses polish notation"""
 
-    if tokens[0] not in func_keys:
-        raise ValueError("Tokens not in polish notation")
-
-    arg_count = func_map[tokens[0]].__code__.co_argcount
-    args = [parse(tokens[arg_n+1:]) if type(tokens[arg_n + 1]) != int else tokens[arg_n + 1] for arg_n in range(arg_count)]
-
-    return func_map[tokens[0]](*args)
+    args = [parse(tokens[arg_n+1:]) if type(tokens[arg_n + 1]) != int else tokens[arg_n + 1] for arg_n in range(2)]
+    return eval("{0}{1}{2}".format(args[0], tokens[0], args[1]))
 
 
 def lex(string):
     return [int(x) if x.isdigit() else x for x in list(string)]
 
-l = "+2*42"
+l = "+2*42-42/23"
+
 print(parse(lex(l)))
